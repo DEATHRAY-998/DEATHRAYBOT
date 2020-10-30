b89dfda862ff6562e4cf42bceecfdb1224583f00
DEATHRAY-998 <73659489+DEATHRAY-998@users.noreply.github.com>
2020-10-30 07:20:31 +0530

    Delete chat.py

--- userbot/plugins/chat.py
+++
@@ -1,4 +0,0 @@
-from telethon import utils
-
-hat_from = event.chat if event.chat else (await event.get_chat()) # telegram MAY not send the chat enity
-chat_title = utils.get_display_name(chat_from)
